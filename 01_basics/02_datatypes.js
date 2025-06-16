eafa2b14b02c296af96a2bd179514539492c4b1f
Keepscrolldown <ankitasingh20323@gmail.com>
2025-06-16 17:39:16 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3)//error but in google it will show as 6
+//different syntax in nodes, we are using node js, not browser
+console.log(3+3) 
+
+console.log("hitesh")  
+
+let name="Ankita"
+let age=18
+let isLoggedIn=false
+let state;
+let temp=null;
+
+//datatypes
+//number=> 2 to power 53
+//bigint Reddit or facebook
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>not defined value;
+//Symbol =>unique 
+
+//object maharathi
+
+console.log(typeof "hitesh")
+
+
+//interview
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
