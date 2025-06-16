067f3a62343cdf2ca1ffae3c15694276939bcdec
Keepscrolldown <ankitasingh20323@gmail.com>
2025-06-16 16:44:03 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="ankita@20323@gmail.com"
+var accoutPassword="12345"//don't use ever
+accountCity="Jaipur"//not a good way but it is possible without variable we can write it
+let accountState;// give undefined if we leave variable like this.
+
+//accountId=2 // not allowed
+
+accountEmail="ac@ac.com"
+accoutPassword="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accoutPassword,accountCity,accountState])
