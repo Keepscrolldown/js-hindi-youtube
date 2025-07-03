8efa952cef088ac48b6cf2b961dd20a63b85c4b5
Ankita singh <ankitasingh20323@gmail.com>
2025-07-03 18:52:23 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+//  console.log(2>1);
+// console.log(2>=1);
+// console.log(2< 1);
+//  console.log(2==1);
+//  console.log(2!=1);
+
+// console.log("2" > 1);//not predictable  //true
+// console.log("02">1); //true
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//conversion problem //true
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+
+//avoid aoid avoid
+
+//=== strict check
+console.log("2"===2);//true
+console.log("2"===2);//false
+
+
+
+
+//don't use two different datatypes
